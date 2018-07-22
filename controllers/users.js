52cd4af8dc5186720e26e79ffbec2ed641a31790
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-22 16:27:39 -0500

    user get and get id route working

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+// const Photo = require('..models/photos');
 
-router.get('/', (req, res, next) => {
-  User.find({}, (err, foundUsers) => {
+router.get('/', (req, res) => {
+  // User.find({}, (err, foundUsers) => {
       res.render('users/users.ejs', {
-        users: foundUsers
-      });
+  //       users: foundUsers
+  //     });
   });
-
 });
 
+// GET ROUTE
+// /user/:id - Page displaying profile information (also our edit page)
+router.get('/:id', (req, res) => {
+  res.send('page displaying profile info / also our edit page')
+})
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+// PUT ROUTE
+// /user/:id/update - (photos are not included in the update request , only profile information);
+
+
+// DELETE ROUTE
+// /user/delete/:id - Delete user account ( Removes all photos owned)
+
+
+
+
 
 module.exports = router;
