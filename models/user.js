18e3952b73ea1450888f696228f72e3ffeb637b3
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-22 13:47:13 -0500

    Added Photos model + linked to User model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-  
+
+const Photos = require('./photo');
+
 const userSchema = mongoose.Schema({
   userName = {type: String, required: true, unique: true},
   password = {type: String, required: true},
