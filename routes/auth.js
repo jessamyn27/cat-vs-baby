687a68c65b67d4c88be9380f94e258f4302079ce
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-22 16:16:21 -0500

    basic Auth routes made

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,28 @@ var router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req,res)=>{
-  res.send('test')
+// Login page 
+router.get('/login', (req,res)=>{
+  res.send('Login page')
+});
+
+// Register Page
+router.get('/register', (req,res)=>{
+  res.send('Reg page')
 })
+
+// Login Request 
+router.post('/login', (req,res)=>{
+  res.send('Req to sign in')
+});
+
+router.post('/register', (req,res)=>{
+  res.send('Req to sign up');
+})
+
+
+
+
 
 module.exports = router;
 
