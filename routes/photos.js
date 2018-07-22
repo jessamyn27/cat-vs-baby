dd57755e3b43db9fdac2b3ad5e9124b819d52ec4
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-22 17:19:46 -0500

    added view for Photos for all babies, and prep coded the find query

--- routes/photos.js
+++ routes/photos.js
@@ -4,8 +4,17 @@ const router = express.Router();
 
 const Photos = require('../models/photo');
 
-router.get('/babies/showall', (req,res)=>{
-  res.send('show all babies routes')
+router.get('/babies/showall', async(req,res,err)=>{
+  // try {
+  //   const allBabies = await Photos.find({babyPhotos: {$exists: true}});
+
+  //   res.render('/photos/allBabies.ejs', {
+  //     babyPhotos: allBabies
+  //   })
+  // } catch (err) {
+  //   res.send(err);
+  // }
+  res.render('photos/allBabies.ejs')
 });
 
 router.get('/cats/showall', (req,res)=>{
