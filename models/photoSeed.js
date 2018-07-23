8c3e3734db6972981051155452d7f6f3d9ecb939
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-23 10:12:22 -0500

    added photo seed

---
+++ models/photoSeed.js
@@ -0,0 +1,75 @@
+const photoSeed = [{
+    catPhotos: [{
+        url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+        description: 'this is cuuuute',
+        rank: 2
+      }, {
+        url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+        description: 'this is cuuuute',
+        rank: 2
+      },
+      {
+        url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+        description: 'this is cuuuute',
+        rank: 2
+      }
+    ],
+    babyPhotos: [{
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }, {
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }, {
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }]
+  },
+  {
+    catPhotos: [{
+      url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+      description: 'this is cuuuute',
+      rank: 2
+    }],
+    babyPhotos: [{
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }]
+  },
+  {
+    catPhotos: [{
+      url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+      description: 'this is cuuuute',
+      rank: 2
+    }],
+    babyPhotos: [{
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }]
+  },
+  {
+    catPhotos: [{
+      url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+      description: 'this is cuuuute',
+      rank: 2
+    }]
+  }, {
+    catPhotos: [{
+      url: 'https://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat-1024x576.jpg',
+      description: 'this is cuuuute',
+      rank: 2
+    }],
+    babyPhotos: [{
+      url: 'https://pbs.twimg.com/profile_images/887312868231852033/bt4qKIjg.jpg',
+      description: 'this is also cuuuute',
+      rank: 1
+    }]
+  }
+]
+
+module.exports = photoSeed;
